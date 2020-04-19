88d9855a257bdb69655d7a3ee43b7d0d8c0a2874
YichengShen <jsszsyc@hotmail.com>
2020-04-18 22:04:03 -0400

    add create new assessment page

--- eval_professor/static/eval_professor/js/create-new-assessment.js
+++ eval_professor/static/eval_professor/js/create-new-assessment.js
@@ -1,2 +1,23 @@
 // Data Picker Initialization
 $('.datepicker').pickadate();
+
+
+// For add and delete question 
+var i = 1;
+$(document).ready(function() {
+    $(".add-more").click(function(){
+        var html = $(".copy").html();
+        $(".after-add-more").after(html);
+        // set dynamic id and name 
+        var input_tag = document.getElementById("question-1");
+        window.i++;
+        console.log(window.i);
+        var name_to_set = 'question-' + i;
+        input_tag.name = name_to_set;
+        input_tag.id = name_to_set;
+    });
+    $("body").on("click",".remove",function(){ 
+        // $(this).parents(".control-group").remove();
+        $(this).parents(".remove-this").remove();
+    });
+});
