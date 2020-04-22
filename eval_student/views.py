854aa17e5aa24b00a71d0f5337d28dd37e6d6aeb
Jange1 <juan.786angel@gmail.com>
2020-04-21 23:07:56 -0400

    Added filter to show only the assesments corresponding to X student

--- eval_student/views.py
+++ eval_student/views.py
@@ -10,6 +10,8 @@ from re import search as regex_search
 import datetime
 
 # Student Dashboard Page
+
+
 def student_dashboard(request):
     return render(request, 'eval_student/student-dashboard.html')
 
@@ -17,14 +19,18 @@ def student_dashboard(request):
 # Peer Assessments Page
 def peer_assessments(request):
     assessment_list = Assessment.objects.all()
+    filtered_list = []
+    for i in assessment_list:
+       if  request.user in i.course.students.all():
+           filtered_list.append(i)
+        
     context = {
-        'assessment_list': assessment_list,
+        'assessment_list': filtered_list,
     }
-    # for i in assessment_list:
-    #     print(i.name, i.end_date, i.questions)
-    return render(request, 'eval_student/peer-assessments.html', context = context)
-
     
+    return render(request, 'eval_student/peer-assessments.html', context=context)
+
+
 # Completed Assessments Page
 def completed_assessments(request):
     return render(request, 'eval_student/completed-assessments.html')
